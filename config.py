38fdb9df996b0e145b6b5419c32569749b16ade3
Anshu <imanshu822@gmail.com>
2023-11-01 00:19:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,11 +15,11 @@ class Config():
     FLASK_ENV = 'development'
     SECRET_KEY = 'secret'
     # Replace these with your ElephantSQL connection details
-    ELEPHANTSQL_USER = 'ocjbyaxs'
-    ELEPHANTSQL_PASSWORD = 'oy3wYnaZ-dIxrydaph_7wgnBl3f3K6jq'
+    ELEPHANTSQL_USER = 'xdyoyyen'
+    ELEPHANTSQL_PASSWORD = 'YWUh2Qz_05ZyzKtTLefPUhxF6e5hzQdi'
     ELEPHANTSQL_HOST = 'suleiman.db.elephantsql.com'
     ELEPHANTSQL_PORT = '5432'
-    ELEPHANTSQL_DATABASE = 'ocjbyaxs'
+    ELEPHANTSQL_DATABASE = 'xdyoyyen'
 
     # Constructing the ElephantSQL connection string
     SQLALCHEMY_DATABASE_URI = f"postgresql://{ELEPHANTSQL_USER}:{ELEPHANTSQL_PASSWORD}@{ELEPHANTSQL_HOST}:{ELEPHANTSQL_PORT}/{ELEPHANTSQL_DATABASE}"
